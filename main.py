8c6120571039b4e81ee74f0bee6c57d1e15b5599
danieltremura <daniel.tremura@tarainteractive.com>
2020-08-31 19:41:07 +0300

    prevent out of bounds

--- main.py
+++ main.py
@@ -7,7 +7,10 @@ import pygame
 pygame.init()
 
 # create the screen
-screen = pygame.display.set_mode((500, 500))
+SCREEN_WIDTH = 900
+SCREEN_HEIGHT = 900
+BACKGROUND_COLOR = (23, 23, 100) # rgb
+screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
 
 # Title and icon
 pygame.display.set_caption("Space Invaders")
@@ -16,13 +19,23 @@ pygame.display.set_icon(icon)
 
 # Player
 playerImg = pygame.image.load('resources/player.png')
-playerImgWidth = playerImg.get_rect().size[0]
-PLAYER_X = int (250 - playerImgWidth / 2)
-PLAYER_Y = 370
+PLAYER_IMG_WIDTH = playerImg.get_rect().size[0]
+# setting initial position
+X_MIDDLE_OF_SCREEN = int (SCREEN_WIDTH / 2 - PLAYER_IMG_WIDTH / 2)
+player_x = X_MIDDLE_OF_SCREEN
+Y_BOTTOM_OF_SCREEN = 90 * SCREEN_HEIGHT / 100
+player_y = Y_BOTTOM_OF_SCREEN
 
-def player():
+# unit of motion
+STEP = 0.3
+player_x_change = 0
+player_y_change = 0
+
+RIGHT_OUT_OF_BOUNDS = SCREEN_WIDTH - PLAYER_IMG_WIDTH
+
+def player(player_x, player_y):
     """    draw player    """
-    screen.blit(playerImg, (PLAYER_X, PLAYER_Y))
+    screen.blit(playerImg, (player_x, player_y))
 
 RUNNING = True
 
@@ -30,6 +43,22 @@ while RUNNING:
     for event in pygame.event.get():
         if event.type == pygame.QUIT:
             RUNNING = False
-    screen.fill((23, 23, 100))
-    player()
+
+        # if keystroke is presed check if whether its right or left
+        if event.type == pygame.KEYDOWN:
+            if event.key == pygame.K_LEFT:
+                player_x_change = -STEP
+            if event.key == pygame.K_RIGHT:
+                player_x_change = STEP
+        if event.type == pygame.KEYUP:
+            if event.key == pygame.K_LEFT or event.key == pygame.K_RIGHT:
+                player_x_change = 0
+    player_x += player_x_change
+
+    if player_x <= 0:
+        player_x = 0
+    elif player_x >= RIGHT_OUT_OF_BOUNDS:
+        player_x = RIGHT_OUT_OF_BOUNDS
+    screen.fill(BACKGROUND_COLOR)
+    player(player_x, player_y)
     pygame.display.update()
