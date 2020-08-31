99df5973052a4b8acfd1b183b9f26e1362ab5757
danieltremura <daniel.tremura@tarainteractive.com>
2020-08-31 14:52:40 +0300

    added player image

--- main.py
+++ main.py
@@ -1,3 +1,6 @@
+"""
+Pygame is a set of Python modules designed for writing games.
+"""
 import pygame
 
 # initialize pygame
@@ -11,11 +14,22 @@ pygame.display.set_caption("Space Invaders")
 icon = pygame.image.load('resources/ufo.png')
 pygame.display.set_icon(icon)
 
-running = True
+# Player
+playerImg = pygame.image.load('resources/player.png')
+playerImgWidth = playerImg.get_rect().size[0]
+PLAYER_X = int (250 - playerImgWidth / 2)
+PLAYER_Y = 370
 
-while running:
+def player():
+    """    draw player    """
+    screen.blit(playerImg, (PLAYER_X, PLAYER_Y))
+
+RUNNING = True
+
+while RUNNING:
     for event in pygame.event.get():
         if event.type == pygame.QUIT:
-            running = False
+            RUNNING = False
     screen.fill((23, 23, 100))
+    player()
     pygame.display.update()
