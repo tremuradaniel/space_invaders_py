e691f6873bc3c1bffb3675e0c96cec53f04ac878
danieltremura <daniel.tremura@tarainteractive.com>
2020-08-31 13:50:46 +0300

    title, logo, background color

--- main.py
+++ main.py
@@ -6,9 +6,16 @@ pygame.init()
 # create the screen
 screen = pygame.display.set_mode((500, 500))
 
+# Title and icon
+pygame.display.set_caption("Space Invaders")
+icon = pygame.image.load('resources/ufo.png')
+pygame.display.set_icon(icon)
+
 running = True
 
 while running:
     for event in pygame.event.get():
         if event.type == pygame.QUIT:
             running = False
+    screen.fill((23, 23, 100))
+    pygame.display.update()
